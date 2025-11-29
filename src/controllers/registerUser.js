ad8bfbcd437c6f8513d82a1bb657d63a9d696e1d
MVCorreas <correasmv@gmail.com>
2025-11-29 18:44:31 -0300

    feat: successfully set prisma and added fx to controllers

--- src/controllers/registerUser.js
+++ src/controllers/registerUser.js
@@ -1,11 +1,9 @@
 import bcrypt from "bcryptjs"; //passwords are encrypted for security. This is irreversible, if we want to compare a user when logging to the user in the database, basically we encrypt the password again using the same algorithm which will return the same output
 import jwt from "jsonwebtoken";
-import { prisma } from "../lib/prisma.js"
+import { prisma } from "../lib/prisma.js";
 
 const registerUser = async (req, res) => {
   const { username, password } = req.body;
-  console.log('body', username, password);
-  
 
   const hashedPassword = bcrypt.hashSync(password, 8);
 
--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client.ts'
+import { PrismaClient } from '@prisma/client'
+import pg from 'pg'
 
-const connectionString = `${process.env.DATABASE_URL}`
-
-const adapter = new PrismaPg({ connectionString })
+const directUrl = process.env.DIRECT_URL
+const pool = new pg.Pool({ connectionString: directUrl })
+const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
 export { prisma }
